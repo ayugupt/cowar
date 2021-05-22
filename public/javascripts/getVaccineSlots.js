82602a0602f2b0b4b2605a1c07d3d3b8ff63a327
kartikeysaran <sarankartikey@gmail.com>
2021-05-22 15:47:08 +0530

    Completed Vaccine SLots

--- public/javascripts/getVaccineSlots.js
+++ public/javascripts/getVaccineSlots.js
@@ -1,47 +1,86 @@
+getStateList();
 function getVaccineSlots() {
     var state = document.getElementById("state").value;
     var district = document.getElementById("district").value;
+    let current_datetime = new Date();
+    let formatted_date = current_datetime.getDate()+"-"+current_datetime.getMonth()+"-"+current_datetime.getFullYear();
     alert("Hello from "+ state +" in "+ district);
-
-    const api_url = 
-      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=624&date=22-05-2021";
-
-    getapi(api_url);  
-  }
-
-
-
-  
-
-async function getapi(url) {
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'authority':'cdn-api.co-vin.in',
-            'method': 'GET',
-            'path': '/api/v2/appointment/sessions/public/calendarByDistrict?district_id=624&date=22-05-2021',
-            'scheme': 'https',
-            'accept': 'application/json, text/plain, */*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9',
-            'if-none-match': 'W/"1118f-kbEadTEib+QM3mHbs1qvFyhPOgQ"',
-            'origin': 'https://www.cowin.gov.in',
-            'referer': 'https://www.cowin.gov.in/',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
-        }})
-        .then(response => response.json())
-        .then(data => {console.log('Success:', data);})
-        .catch((error) => {console.error('Error:', error);});
+    const reqVaccine = new XMLHttpRequest();
+    const url=`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district}&date=${formatted_date}`;
+    reqVaccine.open("GET", url);
+    reqVaccine.addEventListener("load", onLoad);
+    reqVaccine.addEventListener("error", onError);
+    reqVaccine.send();
+    
     
 }
 
-// Function to define innerHTML for HTML table
-function show(data) {
-    
-    document.getElementById("employees").innerHTML = tab;
-}  
+function onLoad() {
+    var response = this.responseText;
+    var parsedResponse = JSON.parse(response);
+     
+
+    document.getElementById("availableSlots").innerHTML = 
+    `
+    ${parsedResponse.centers.map(function(slot) {
+       return(
+        `
+       <div class="Slot">
+          <h3 class="slot-hosp-name">${slot.name}</h3>
+       </div>
+        `
+       ) 
+       ;
+    }).join("")}
+    `;
+
+}
+
+function onError(){
+    console.log("There is an error in getting the data");
+}
+
+function getStateList() {
+    const reqVaccine = new XMLHttpRequest();
+    const url='https://cdn-api.co-vin.in/api/v2/admin/location/states';
+    reqVaccine.open("GET", url);
+    reqVaccine.addEventListener("load", updateStatesList);
+    reqVaccine.addEventListener("error", onError);
+    reqVaccine.send();
+
+}
+
+function updateStatesList() {
+    var response = this.responseText;
+    var parsedResponse = JSON.parse(response);
+    var stateList = document.getElementById('states');
+
+    for (var i = 0; i < parsedResponse.states.length; i++) {
+        // POPULATE SELECT ELEMENT WITH JSON.
+        stateList.innerHTML = stateList.innerHTML +
+            '<option value="' + parsedResponse.states[i]['state_id'] + '">' + parsedResponse.states[i]['state_name'] + '</option>';
+    }
+}
+
+function getDistrictList(state) { 
+    const reqVaccine = new XMLHttpRequest();
+    const url=`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state.value}`;
+    reqVaccine.open("GET", url);
+    reqVaccine.addEventListener("load", updateDistrictList);
+    reqVaccine.addEventListener("error", onError);
+    reqVaccine.send();
+}
+
+function updateDistrictList() {
+    var response = this.responseText;
+    var parsedResponse = JSON.parse(response);
+    var districtList = document.getElementById('districts');
+
+    for (var i = 0; i < parsedResponse.districts.length; i++) {
+        // POPULATE SELECT ELEMENT WITH JSON.
+        districtList.innerHTML = districtList.innerHTML +
+            '<option value="' + parsedResponse.districts[i]['district_id'] + '">' + parsedResponse.districts[i]['district_name'] + '</option>';
+    }
+}
+
+
