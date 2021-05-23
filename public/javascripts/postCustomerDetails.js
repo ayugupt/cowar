fce6af3d76a5eb2edd456b5eb71155c7093982ac
ayugupt <ayush.gupta.10301@gmail.com>
2021-05-23 12:07:58 +0530

    fixed home issues

--- public/javascripts/postCustomerDetails.js
+++ public/javascripts/postCustomerDetails.js
@@ -19,4 +19,4 @@ function onPostRequestError(err){
     //alert("There was an error, please try again")
 }
 
-postRequest("gametvgs123@gmail.com", 650, 201301);
+//postRequest("gametvgs123@gmail.com", 650, 201301);
